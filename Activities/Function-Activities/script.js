d6aad85c6fd8c3cac9e0c24b36e38c562bfc1ebb
pransinme <bocalafrancinemae18@gmail.com>
2025-03-24 16:58:44 +0800

    activities

---
+++ Activities/Function-Activities/script.js
@@ -0,0 +1,15 @@
+function whatShallWear (temp){
+    if (temp < 60) {
+        console.log("Wear a jacket");
+     } else if (temp < 70) {
+        console.log("Wear a t-shirt");
+    } else {
+        console.log("Wear a sweater");
+     }
+
+}
+
+
+whatShallWear(50);
+whatShallWear(60);
+whatShallWear(80);
